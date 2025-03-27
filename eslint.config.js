982db367e3092e0bdce2c9f1080c6c46364a5222
loivdVFA <loivd001@gmail.com>
2025-03-28 00:28:36 +0700

    update

--- eslint.config.js
+++ eslint.config.js
@@ -20,7 +20,7 @@ export default tseslint.config(
     rules: {
       ...reactHooks.configs.recommended.rules,
       "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": "error",
       "quotes": ["error", "single"]
     },
   },
